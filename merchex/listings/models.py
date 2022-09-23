d7ec4d23972da4b4b63b6653dd6354da32c81ed1
Ambroisehdn <ambroisehdn@labdev.com>
2022-09-23 14:29:45 +0200

    migration update concept, admin panel concept

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -8,10 +8,20 @@ class Band(models.Model) :
     class Genre(models.TextChoices) :
         
         HIP_HOP = "HH"
+        ALTERNATIVE_ROCK = "AR"
+        SYNTH_POP = "SP"
+        
+    class Type(models.TextChoices) :
+        
+        RECORDS = "RC"
+        CLOTHINGS = "CH"
+        POSTERS = "PT"
+        MISCELLANEOUS = "ML"
+        
+    genre = models.fields.CharField(choices=Genre.choices,max_length=5)
+    
     name = models.fields.CharField(max_length=100)
-    
-    genre = models.fields.CharField(max_length=50)
-    
+        
     biography = models.fields.CharField(max_length=1000)
     
     year_formed = models.fields.IntegerField(
@@ -21,3 +31,11 @@ class Band(models.Model) :
     active = models.fields.BooleanField(default=True)
     
     official_homepage = models.fields.URLField(null=True,blank=True)
+    
+    description = models.fields.CharField(max_length=1000)
+    
+    sold = models.fields.BooleanField(default=False)
+    
+    year = models.fields.IntegerField(null=True)
+    
+    type = models.fields.CharField(choices=Type.choices,max_length=10)
